51106b04289f8801349c7c3e0d7d09448a8a31a1
Romiras <298544+romiras@users.noreply.github.com>
2021-05-17 15:50:18 +0300

    Output results through pipe instead of holding all found results in memory. I.e. don't wait until all results found. Usage:  `fsql "<SQL>" | head -20` or `fsql "<SQL>" | less`

--- fsql.go
+++ fsql.go
@@ -3,9 +3,11 @@ package fsql
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/romiras/fsql/parser"
+	"github.com/romiras/fsql/query"
 )
 
 // Run parses the input and executes the resultant query.
@@ -17,39 +19,54 @@ func Run(input string) error {
 
 	// Find length of the longest name to normalize name output.
 	var max = 0
-	var results = make([]map[string]interface{}, 0)
+	var results = make(chan map[string]interface{})
+	errChan := make(chan error)
 
-	err = q.Execute(
-		func(path string, info os.FileInfo, result map[string]interface{}) {
-			results = append(results, result)
-			if !q.HasAttribute("name") {
-				return
-			}
-			if s, ok := result["name"].(string); ok && len(s) > max {
-				max = len(s)
-			}
-		},
-	)
-	if err != nil {
-		return err
+	go func() {
+		err = q.Execute(
+			func(path string, info os.FileInfo, result map[string]interface{}) {
+				results <- result
+				if !q.HasAttribute("name") {
+					return
+				}
+				if s, ok := result["name"].(string); ok && len(s) > max {
+					max = len(s)
+				}
+			},
+		)
+		if err != nil {
+			errChan <- err
+		}
+		close(results)
+		close(errChan)
+	}()
+
+	printResults(q, results, os.Stdout)
+
+	for err := range errChan {
+		if err != nil {
+			return err
+		}
 	}
 
-	for _, result := range results {
+	return nil
+}
+
+func printResults(q *query.Query, results chan map[string]interface{}, w io.Writer) {
+	for result := range results {
 		var buf bytes.Buffer
 		for j, attribute := range q.Attributes {
 			// If the current attribute is "name", pad the output string by `max`
 			// spaces.
 			format := "%v"
 			if attribute == "name" {
-				format = fmt.Sprintf("%%-%ds", max)
+				format = fmt.Sprintf("%%-%ds", len(result[attribute].(string))) // 'max' not used
 			}
 			buf.WriteString(fmt.Sprintf(format, result[attribute]))
 			if j != len(q.Attributes)-1 {
 				buf.WriteString("\t")
 			}
 		}
-		fmt.Printf("%s\n", buf.String())
+		fmt.Fprintf(w, "%s\n", buf.String())
 	}
-
-	return nil
 }
